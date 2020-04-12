2ab6bb3d442dfa871a36c74f736ab03c98111b49
devjoco <devonjoconnor@gmail.com>
2020-04-12 00:53:17 -0400

    Validates moves, accepting col or row first

--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+import re, secrets
+
 class TicTacToe:
     ''' TicTacToe Class '''
+    SYMBOLS = ('X', 'O')
 
     def __init__(self, size):
         self.board = [[' ' for _ in range(size)] for _ in range(size)]
+        self.moves = 0
+        self.player = secrets.randbelow(100) % 2
 
     def getWinner(self):
         # Check horizontal wins
@@ -38,22 +43,51 @@ class TicTacToe:
         return None
 
     def showBoard(self):
-        b = ''
-        rowDiv  = ''.join([('─', '┼')[i%2] for i in range(len(self.board)*2-1)]) + '\n'
+        b = '\n\t  ' + ' '.join([chr(65+i) for i in range(len(self.board))]) + '\n'
+        rowDiv = '\t  ' + ''.join([('─','┼')[i%2] for i in range(len(self.board)*2-1)]) + '\n'
         for i, row in enumerate(self.board):
             if i != 0:
                 b += rowDiv
             for j, cell in enumerate(row):
-                leading  = ''   if j == 0          else '│'
+                leading  = f'\t{i+1} '   if j == 0          else '│'
                 trailing = '\n' if j == len(row)-1 else ''
                 value = '.' if cell == ' ' else cell
                 b += f'{leading}{value}{trailing}'
         print(b)
 
+    def spotOpen(self, r, c):
+        return self.board[r][c] == ' '
+
+    def getMove(self, msg="Where would you like to go? "):
+        validCol  = '[' + ''.join([chr(x+97) for x in range(len(self.board))])        + ']'
+        validRow  = '[' + ''.join(map(str, [x for x in range(1, len(self.board)+1)])) + ']'
+        ansPattern = validCol + validRow + '|' + validRow + validCol
+        ans = ''.join(input(msg).strip().split())
+        while True:
+            while not re.fullmatch(ansPattern, ans.lower()):
+                msg = "Choose a valid row and column. "
+                ans = ''.join(input(msg).strip().split())
+            try:
+                r = int(ans[0]) - 1
+                c = ord(ans[1].lower()) - 97
+            except ValueError:
+                r = ord(ans[0].lower()) - 97
+                c = int(ans[1]) - 1
+            if not self.spotOpen(r,c):
+                print("That spot has already been taken.")
+                ans = ''
+                continue
+            else:
+                break
+        return (r,c)
+    
+
 if __name__ == '__main__':
-    game = TicTacToe(3)
+    print("     --Tic-Tac-Toe--")
+    game  = TicTacToe(3)
     while game.getWinner() == None:
         game.showBoard()
-        move = input("Show board again? ")
+        row, col = game.getMove()
+
 
 
