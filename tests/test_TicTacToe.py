3c7d8b982aa67eea6a75133ae16ac69ce45c6c98
devjoco <devonjoconnor@gmail.com>
2020-04-19 13:04:21 -0400

    Add tests: convertRowCol, first move; refactoring
    Had an issue where the correct row/col was updated, but incorrect row/col was reported. Added
    test cases to correct this.
    
    Testing that the correct player makes the first move using
    mocked input
    
    Testing a range of parameters for instantiating TicTacToe's with subTest
    
    Refactored assertEqual()'s to check the turn equals a Turn instance
    rather than just checking the value of the turn against a string value

--- tests/test_TicTacToe.py
+++ tests/test_TicTacToe.py
@@ -1,57 +1,124 @@
 #!/usr/bin/env python3
 import unittest
+from unittest import TestCase, mock, expectedFailure
 from tictactoe.TicTacToe import TicTacToe as TTT
 
 
-class Test_Default_Game(unittest.TestCase):
+class Test_Utility_Funcs(TestCase):
+
+    def test_convert__r0_c0(self):
+        spot = TTT.convertRowCol(0, 0)
+        self.assertEqual(spot, "A1")
+
+    def test_convert_r0_c1(self):
+        spot = TTT.convertRowCol(0, 1)
+        self.assertEqual(spot, "B1")
+        
+    def test_convert_r0_c2(self):
+        spot = TTT.convertRowCol(0, 2)
+        self.assertEqual(spot, "C1")
+
+    def test_convert_r1_c0(self):
+        spot = TTT.convertRowCol(1, 0)
+        self.assertEqual(spot, "A2")
+
+    def test_convert_r1_c1(self):
+        spot = TTT.convertRowCol(1, 1)
+        self.assertEqual(spot, "B2")
+
+    def test_convert_r1_c2(self):
+        spot = TTT.convertRowCol(1, 2)
+        self.assertEqual(spot, "C2")
+
+    def test_convert_r2_c0(self):
+        spot = TTT.convertRowCol(2, 0)
+        self.assertEqual(spot, "A3")
+
+    def test_convert_r2_c1(self):
+        spot = TTT.convertRowCol(2, 1)
+        self.assertEqual(spot, "B3")
+
+    def test_convert_r2_c2(self):
+        spot = TTT.convertRowCol(2, 2)
+        self.assertEqual(spot, "C3")
+
+
+class Test_Single_Player(TestCase):
+    @mock.patch('tictactoe.TicTacToe.input', create=True)
+    def test_comp_goes_after_player(self, mocked_input):
+        mocked_input.side_effect = ['a1']
+        self.game = TTT(size=3, first='p')
+        self.game.make_move()
+        self.assertEqual(self.game.turn, self.game.Turn.PLAYER)
+
+    def tearDown(self):
+        self.game = None
+
+
+class Test_TTT_Parameters(TestCase):
+    def test_game_size(self):
+        for i in range(20):
+            with self.subTest(i=i):
+                self.game = TTT(size=i)
+                self.assertEqual(self.game.size, i)
+
+    def test_first_move(self):
+        for c in 'pcr':
+            with self.subTest(first=c):
+                self.game = TTT(first=c)
+                self.assertIn(self.game.turn, 
+                              list(self.game.Turn.__members__.values()))
+
+
+class Test_Default_Game(TestCase):
     def setUp(self):
         self.game = TTT()
 
     def test_player_choice(self):
-        self.assertEqual(self.game.turn.value, 'X')
+        self.assertEqual(self.game.turn, self.game.Turn.PLAYER)
 
     def test_default_size_3(self):
         self.assertEqual(self.game.size, 3)
 
     def test_advance_turn(self):
-        self.assertEqual(self.game.turn.value, 'X')
+        self.assertEqual(self.game.turn, self.game.Turn.PLAYER)
         self.game._advance_turn()
-        self.assertEqual(self.game.turn.value, 'O')
+        self.assertEqual(self.game.turn, self.game.Turn.COMPUTER)
 
     def test_hori_wins(self):
         for row in range(self.game.size):
             self.game._reset_board()
             self.assertEqual(self.game.get_winner(), None)
             for col in range(self.game.size):
-                self.game.board[row][col] = 'X'
-            self.assertEqual(self.game.get_winner(), 'X')
+                self.game.board[row][col] = self.game.Turn.PLAYER.value
+            self.assertEqual(self.game.get_winner(), self.game.Turn.PLAYER.value)
 
     def test_vert_wins(self):
         for col in range(self.game.size):
             self.game._reset_board()
             self.assertEqual(self.game.get_winner(), None)
             for row in range(self.game.size):
-                self.game.board[row][col] = 'O'
-            self.assertEqual(self.game.get_winner(), 'O')
+                self.game.board[row][col] = self.game.Turn.COMPUTER.value
+            self.assertEqual(self.game.get_winner(), self.game.Turn.COMPUTER.value)
 
     def test_diag_tl_br_win(self):
         for row in range(self.game.size):
             self.assertEqual(self.game.get_winner(), None)
             for col in range(self.game.size):
                 if row == col:
-                    self.game.board[row][col] = 'X'
-        self.assertEqual(self.game.get_winner(), 'X')
+                    self.game.board[row][col] = self.game.Turn.PLAYER.value
+        self.assertEqual(self.game.get_winner(), self.game.Turn.PLAYER.value)
 
     def test_diag_bl_tr_win(self):
         for row in range(self.game.size):
             self.assertEqual(self.game.get_winner(), None)
             for col in range(self.game.size):
                 if row + col == self.game.size - 1:
-                    self.game.board[row][col] = 'O'
-        self.assertEqual(self.game.get_winner(), 'O')
+                    self.game.board[row][col] = self.game.Turn.COMPUTER.value
+        self.assertEqual(self.game.get_winner(), self.game.Turn.COMPUTER.value)
 
     def test_tie_game(self):
-        symbs = ('X', 'O')
+        symbs = (self.game.Turn.PLAYER.value, self.game.Turn.COMPUTER.value)
         for row in range(self.game.size - 1):
             for col in range(self.game.size):
                 self.game.board[row][col] = symbs[col % 2]
@@ -63,20 +130,20 @@ class Test_Default_Game(unittest.TestCase):
         del self.game
 
 
-class Test_Custom_Game(unittest.TestCase):
+class Test_Custom_Game(TestCase):
     def setUp(self):
         self.game = TTT(5, 'c')
 
     def test_player_choice(self):
-        self.assertEqual(self.game.turn.value, 'O')
+        self.assertEqual(self.game.turn, self.game.Turn.COMPUTER)
 
     def test_custom_game_size(self):
         self.assertEqual(self.game.size, 5)
 
     def test_advance_turn(self):
-        self.assertEqual(self.game.turn.value, 'O')
+        self.assertEqual(self.game.turn, self.game.Turn.COMPUTER)
         self.game._advance_turn()
-        self.assertEqual(self.game.turn.value, 'X')
+        self.assertEqual(self.game.turn, self.game.Turn.PLAYER)
 
     def tearDown(self):
         del self.game
